427f7caf74d3c2f400cbd3c25978e104df5f28e1
sh.seo <sh.seo891114@xcurenet.com>
2023-11-14 16:21:43 +0900

    Revert "임시저장"
    This reverts commit c3a987fd64b8c178de1961157537760244304201.

--- src/main/resources/static/js/conditionNew.js
+++ src/main/resources/static/js/conditionNew.js
@@ -358,7 +358,7 @@ var con = {
 		var condition = {};
 
 		if(!document.getElementById('msg_condition_menu') || !$('#msg_condition_saver').hasClass('condition_menu_unselected')){
-			condition.query = $('#elsQueryText').val();
+			condition.query = $('#solrQueryText').val();
 			condition.sort = $('#messageSort').val();
 			
 			if(pageType == 'M') condition.svc1_not = 'U';
@@ -558,10 +558,9 @@ var con = {
 				$('#enddatepicker').data("DateTimePicker").date(condition.endDt.toDate());
 			}
 
-			$('#elsQueryText').val(condition.query);
+			$('#solrQueryText').val(condition.query);
 		}
 		$('#searchStrInput').val(condition.searchStr);
-		console.log(condition.searchStr);
 		$('#searchField').selectpicker('val', stringToArray(condition.searchField) );
 		$('#serviceType').selectpicker('val', stringToArray(condition.serviceType) );
 		
