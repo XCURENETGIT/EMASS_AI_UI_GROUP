1a917f7b859398a1cbd2cb2fd6a86a4f502a9019
sh.seo <sh.seo891114@xcurenet.com>
2024-01-04 11:28:14 +0900

    파일 다운로드 수정

--- src/main/java/com/xcurenet/emass/message/web/EmsAttachDownload.java
+++ src/main/java/com/xcurenet/emass/message/web/EmsAttachDownload.java
@@ -31,129 +31,24 @@ public class EmsAttachDownload {
 	public MinioFileAdapter minioFileAdapter;
 
 
-//	@Description("파일 다운로드")
-//	public void download(final EmsAttachVO attach, final HttpServletRequest request, final HttpServletResponse response, final String prediction) throws Exception {
-//		response.setCharacterEncoding(Common.UTF8);
-//		if (attach == null) {
-//			log.warn("file not found..attach is null");
-//			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-//			return;
-//		}
-//		InputStream in = null;
-//		OutputStream out = null;
-//		try {
-//			out = response.getOutputStream();
-//			response.setContentType("application/octet-stream");
-//			response.setHeader("Content-Transfer-Encoding", "binary");
-//			response.setHeader("Connection", "close");
-//
-//			Cookie f = new Cookie("fileDownload", "true");
-//			f.setMaxAge(1 * 24 * 60 * 60);
-//			f.setPath("/");
-//			response.addCookie(f);
-//
-//			String path = attach.getAttachPath();
-//			String harPath = attach.getAttachHarPath();
-//			log.info("path:{}, harPath:{}", path, harPath);
-//			in = getAttach(path, harPath);
-//			if (in == null) {
-//				log.warn("file not found..attach {} {} {}", attach.getMsgId(), attach.getAttachId(), attach.getAttachPath());
-//				response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-//				response.addCookie(new Cookie("fileDownload", "false"));
-//			} else {
-//				if (Common.isEquals(prediction, "Y")) attach.setAttachName(attach.getAttachName() + "." + (Common.isEquals(attach.getAttachExt(), "unknown") ? "txt" : attach.getAttachExt()));
-//				response.setHeader("Content-Disposition", "attachment; filename=\"" + Common.getEncodingFileName(request, attach.getAttachName()) + "\"");
-//				if (attach.getAttachSize() <= Integer.MAX_VALUE) response.setContentLength((int) attach.getAttachSize());
-//				IOUtils.copyLarge(in, out);
-//			}
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-//			log.error("file download error.. attach {} {}", attach.getMsgId(), attach.getAttachId());
-//			response.addCookie(new Cookie("fileDownload", "false"));
-//		} finally {
-//			IOUtils.closeQuietly(in);
-//			IOUtils.closeQuietly(out);
-//			response.flushBuffer();
-//		}
-//	}
-
-	@Description("파일 다운로드")
+	@Description("파일 다운로드") /* 단건 */
 	public void download(final EmsAttachVO attach, final HttpServletRequest request, final HttpServletResponse response, final String prediction) throws Exception {
 		response.setCharacterEncoding(Common.UTF8);
+		if (attach.getAttachSize() <= Integer.MAX_VALUE) response.setContentLength((int) attach.getAttachSize());
 		if (attach == null) {
 			log.warn("file not found..attach is null");
 			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
 			return;
 		}
 		String objectName = attach.getAttachPath();
-		String fileName = attach.getAttachName();
+		log.info("path:{}", objectName);
+		/* prediction */
+		String fileName = (Common.isEquals(prediction, "Y")) ? attach.getAttachName() + "." + (Common.isEquals(attach.getAttachExt(), "unknown") ? "txt" : attach.getAttachExt()) : attach.getAttachName();
 		minioFileAdapter.fileDownload(objectName, fileName, request, response);
 	}
 
 
-
-//	@Description("파일 다운로드")
-//	public void download(final List<EmsAttachVO> attachs, final HttpServletRequest request, final HttpServletResponse response, final String prediction) throws Exception {
-//		response.setCharacterEncoding(Common.UTF8);
-//		if (attachs == null || attachs.size() == 0) {
-//			log.warn("file not found..attach is null");
-//			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-//			return;
-//		}
-//		if (attachs.size() == 1) {
-//			download(attachs.get(0), request, response, prediction);
-//			return;
-//		}
-//
-//		OutputStream out = null;
-//		ArchiveOutputStream os = null;
-//		try {
-//			out = response.getOutputStream();
-//			response.setContentType("application/octet-stream");
-//			response.setHeader("Content-Transfer-Encoding", "binary");
-//			response.setHeader("Connection", "close");
-//			response.setHeader("Content-Disposition", "attachment; filename=\"" + Common.getDateTimeFormat() + "_attachs.zip\"");
-//
-//			os = new ArchiveStreamFactory().createArchiveOutputStream("zip", out);
-//			for (EmsAttachVO attach : attachs) {
-//				InputStream in = null;
-//				try {
-//					String path = attach.getAttachPath();
-//					String harPath = attach.getAttachHarPath();
-//					log.info("path:{}, harPath:{}", path, harPath);
-//					in = getAttach(path, harPath);
-//					if (in == null) continue;
-//					if (Common.isEquals(prediction, "Y")) attach.setAttachName(attach.getAttachName() + "." + attach.getAttachExt());
-//
-//					String subject = EmsReDefined.reSubject(getFileName(attach));
-//					String dir = Common.getEDCFileName(attach.getCtime(), attach.getUserId(), attach.getName(), subject, attach.getMsgId());
-//
-//					os.putArchiveEntry(new ZipArchiveEntry(dir + File.separatorChar + attach.getAttachName()));
-//
-//					IOUtils.copy(in, os);
-//					os.closeArchiveEntry();
-//				} catch (ClientAbortException e) {
-//					throw new Exception(e);
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				} finally {
-//					IOUtils.closeQuietly(in);
-//				}
-//			}
-//
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-//		} finally {
-//			IOUtils.closeQuietly(os);
-//			IOUtils.closeQuietly(out);
-//			response.flushBuffer();
-//		}
-//	}
-
-
-	@Description("파일 다운로드")
+	@Description("파일 다운로드") /* 다건 (압축) */
 	public void download(final List<EmsAttachVO> attachs, final HttpServletRequest request, final HttpServletResponse response, final String prediction) throws Exception {
 		response.setCharacterEncoding(Common.UTF8);
 		if (attachs == null || attachs.size() == 0) {
@@ -161,64 +56,56 @@ public class EmsAttachDownload {
 			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
 			return;
 		}
-		if (attachs.size() == 1) {
-			download(attachs.get(0), request, response, prediction);
-			return;
-		}
 
 		OutputStream out = null;
 		ArchiveOutputStream os = null;
-		try {
-			out = response.getOutputStream();
-			response.setContentType("application/octet-stream");
-			response.setHeader("Content-Transfer-Encoding", "binary");
-			response.setHeader("Connection", "close");
-			response.setHeader("Content-Disposition", "attachment; filename=\"" + Common.getDateTimeFormat() + "_attachs.zip\"");
+		if (attachs.size() == 1) download(attachs.get(0), request, response, prediction);
+		else if(attachs.size() > 1) {
+			try {
+				out = response.getOutputStream();
+				response.setContentType("application/octet-stream");
+				response.setHeader("Content-Transfer-Encoding", "binary");
+				response.setHeader("Connection", "close");
+				response.setHeader("Content-Disposition", "attachment; filename=\"" + Common.getDateTimeFormat() + "_attachs.zip\"");
 
-			os = new ArchiveStreamFactory().createArchiveOutputStream("zip", out);
-			for (EmsAttachVO attach : attachs) {
-				System.out.println(attach.getAttachName());
-				System.out.println(attach.getAttachPath());
-				InputStream in = null;
-				try {
-					String path = attach.getAttachPath();
-					String harPath = attach.getAttachHarPath();
-					log.info("path:{}, harPath:{}", path, harPath);
-					in = getAttach(path, harPath);
-					in = minioFileAdapter.findFile(attach.getAttachPath());
-					if (in == null) continue;
-					if (Common.isEquals(prediction, "Y")) attach.setAttachName(attach.getAttachName() + "." + attach.getAttachExt());
+				os = new ArchiveStreamFactory().createArchiveOutputStream("zip", out);
+				for (EmsAttachVO attach : attachs) {
+					InputStream in = minioFileAdapter.findFile(attach.getAttachPath());
+					try {
+						String path = attach.getAttachPath();
+//						String harPath = attach.getAttachHarPath();
+						log.info("path:{}", path);
+						if (in == null) continue;
+						if (Common.isEquals(prediction, "Y")) attach.setAttachName(attach.getAttachName() + "." + attach.getAttachExt());
+						String fileName = (Common.isEquals(prediction, "Y")) ? attach.getAttachName() + "." + (Common.isEquals(attach.getAttachExt(), "unknown") ? "txt" : attach.getAttachExt()) : attach.getAttachName();
+						String subject = EmsReDefined.reSubject(getFileName(attach));
+						String dir = Common.getEDCFileName(attach.getCtime(), attach.getUserId(), attach.getName(), subject, attach.getMsgId());
+						os.putArchiveEntry(new ZipArchiveEntry(dir + File.separatorChar + fileName));
+						IOUtils.copy(in, os);
+						os.closeArchiveEntry();
+					} catch (ClientAbortException e) {
+						throw new Exception(e);
+					} catch (Exception e) {
+						e.printStackTrace();
+					} finally {
+						IOUtils.closeQuietly(in);
+					}
 
-					String subject = EmsReDefined.reSubject(getFileName(attach));
-					String dir = Common.getEDCFileName(attach.getCtime(), attach.getUserId(), attach.getName(), subject, attach.getMsgId());
-
-					os.putArchiveEntry(new ZipArchiveEntry(dir + File.separatorChar + attach.getAttachName()));
-
-					IOUtils.copy(in, os);
-					os.closeArchiveEntry();
-				} catch (ClientAbortException e) {
-					throw new Exception(e);
-				} catch (Exception e) {
-					e.printStackTrace();
-				} finally {
-					IOUtils.closeQuietly(in);
 				}
 			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-		} finally {
-			IOUtils.closeQuietly(os);
-			IOUtils.closeQuietly(out);
-			response.flushBuffer();
+			catch (Exception e) {
+				e.printStackTrace();
+				response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			} finally {
+				IOUtils.closeQuietly(os);
+				IOUtils.closeQuietly(out);
+				response.flushBuffer();
+			}
 		}
 	}
 
-	/*
-	 * public InputStream getAttach(String path, String harPath) { return
-	 * Common.getHdfsAttach(path, harPath); }
-	 */
+
+
 
 	public InputStream getAttach(String path, String harPath) {
 		SFTPUtil ftp = null;
@@ -258,6 +145,7 @@ public class EmsAttachDownload {
 			IOUtils.closeQuietly(in);
 			if (ftp != null && ftp.isConnected()) ftp.disconnection();
 		}
+
 		return null;
 	}
 
@@ -271,5 +159,8 @@ public class EmsAttachDownload {
 		msg.setPath(edc.getPath());
 		return msg;
 	}
-}
 
+
+
+
+}
