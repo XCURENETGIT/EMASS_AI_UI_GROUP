ddc3b3b5a8029dec863b286668a5bf2e3757a1a7
jochangmin <jochangmin22@xcurenet.com>
2024-01-20 01:22:21 +0900

    1. 패키지 수정

---
+++ src/main/resources/com/xcurenet/sqlmap/mappers/sql/procedure.sql
@@ -0,0 +1,12 @@
+DROP PROCEDURE IF EXISTS ALTER_TB;
+
+CREATE PROCEDURE `ALTER_TB`(IN my_table varchar(50), IN col_name varchar (50), IN sql_query varchar (200))
+BEGIN
+    IF (SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = my_table AND COLUMN_NAME = col_name AND TABLE_SCHEMA = 'EMASSAI')=0 THEN
+        BEGIN
+            SET @ddl = sql_query;
+                PREPARE STMT FROM @ddl;
+                EXECUTE STMT;
+        END;
+    END IF;
+END;
