d42d48eb3ef28e6a83a638bf5e12577a2cbfe0dd
문수현 <sh.moon@xcurenet.com>
2024-03-20 09:49:45 +0900

    System Settings > 메일서버 > 메일 전송 디버깅 미동작

--- src/main/java/com/xcurenet/EmassproApplication.java
+++ src/main/java/com/xcurenet/EmassproApplication.java
@@ -1,21 +1,48 @@
 package com.xcurenet;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 
+
+@Log4j2
 @EnableScheduling
 @ServletComponentScan
 @EnableAsync
 @SpringBootApplication
 public class EmassproApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
+		setSystemOutAndErrToLog();
 		SpringApplication.run(EmassproApplication.class, args);
 	}
 
-}
 
+	public static void setSystemOutAndErrToLog() {
+		System.setOut(createLoggingProxy(System.out));
+	}
+
+	public static PrintStream createLoggingProxy(final PrintStream realPrintStream) {
+		return new PrintStream(realPrintStream) {
+			public void print(final String msg) {
+				realPrintStream.print(msg);
+				StackTraceElement[] stacks = Thread.currentThread().getStackTrace();
+				StackTraceElement stack = null;
+				for (int i = 0; i < stacks.length; i++) {
+					if (i == 3) {
+						stack = stacks[i];
+					}
+				}
+				if (stack != null) {
+					log.info("{}", msg);
+				}
+			}
+		};
+	}
+}
