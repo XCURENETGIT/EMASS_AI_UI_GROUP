7240be9e0a49d5bf3a5c4c193f8a52e52fff4612
sh.seo <sh.seo891114@xcurenet.com>
2024-04-01 14:52:34 +0900

    checked 필드 누락 확인

--- src/main/java/com/xcurenet/emass/message/service/SolrCheckedVO.java
+++ src/main/java/com/xcurenet/emass/message/service/SolrCheckedVO.java
@@ -29,6 +29,18 @@ public class SolrCheckedVO {
 		@Field("readId")
 		private String readId; //메시지 개봉 운용자 아이디
 
+
+		@Field("readTime_yyyy")
+		private String readTime_yyyy;
+		@Field("readTime_yyyymm")
+		private String readTime_yyyymm;
+		@Field("readTime_yyyymmdd")
+		private String readTime_yyyymmdd;
+		@Field("readTime_yyyymmddhh")
+		private String readTime_yyyymmddhh;
+		@Field("readTime_hh")
+		private String readTime_hh;
+
 		@Field("readTime")
 		@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
 		private DateTime readTime; //메시지 개봉일
