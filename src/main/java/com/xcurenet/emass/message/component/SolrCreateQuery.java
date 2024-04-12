7d1b25ea8f8c04f1cc16cb8bdad723566578e3e1
sh.seo <sh.seo891114@xcurenet.com>
2024-04-12 13:32:00 +0900

    .

--- src/main/java/com/xcurenet/emass/message/component/SolrCreateQuery.java
+++ src/main/java/com/xcurenet/emass/message/component/SolrCreateQuery.java
@@ -1420,7 +1420,7 @@ public class SolrCreateQuery {
 	public String specialCharsCheck(String str){
 		String result = str;
 		/* 특수문자 처리 */
-		result  =  result.replaceAll("[[\\\\]=/&:><!^~/[\"]\\[\\]\\(\\)\\{\\}]", "\\\\"+"$0");
+		result  =  result.replaceAll("[[\\\\]=/&:><!^~/[\"]\\{\\}]", "\\\\"+"$0");
 		return result;
 	}
 
