1046ad4bf05a8942af94d6212ff4544ecebf49b3
김민경 <mk.kim@xcurenet.com>
2024-04-29 18:11:20 +0900

    insert, patch data 수정

--- src/main/resources/com/xcurenet/sqlmap/mappers/sql/patch_data.sql
+++ src/main/resources/com/xcurenet/sqlmap/mappers/sql/patch_data.sql
@@ -658,6 +658,7 @@ INSERT INTO UI_MENU (`MENU_ID`, `MENU_DEFAULT_NAME`, `P_MENU_ID`, `PKG_TYPE`, `M
 ('POLICY_NOLOG', '데이터 미로깅 정책', 'POLICY_MGMT', 'L', 'S', 'uacs/filterInfo.do', 'fa fa-unlink', 1, 'Y', NULL),
 ('POLICY_SETUP', '정책 설정', NULL, 'L', 'S', NULL, 'glyphicon glyphicon-eye-close', 6, 'Y', '/img/ico_gnb_06.png'),
 ('REGEX_PATTERN', '정규식 패턴 관리', 'MONITOR_MGMT', 'L', 'M', 'ems/regexPatternInfo.do', 'fa fa-building', 4, 'Y', NULL),
+('PATTERN_KEYWORD', '패턴 관리', 'MONITOR_MGMT', 'L', 'M', 'ems/patternKeyword.do', 'fa fa-building', 5, 'Y', NULL),
 ('RELATION_KEYWORD', '연관 키워드 관리', 'MONITOR_MGMT', 'L', 'M', 'ems/relationKeyword.do', 'fa fa-building', 3, 'Y', NULL),
 ('REPORT_CONTENT', '컨텐츠 보고서', 'DATA_REPORT', 'L', 'M', 'report/contentReport.do', 'glyphicon glyphicon-list-alt', 2, 'Y', NULL),
 ('REPORT_DEVICE', '장비 운용 보고서', 'DATA_REPORT', 'L', 'M', 'report/deviceReport.do', 'fa fa-area-chart', 3, 'N', NULL),
