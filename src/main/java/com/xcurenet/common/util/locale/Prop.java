aa7a7aa6b70db87d22a8869aa5605b4cb8976796
김민경 <mk.kim@xcurenet.com>
2024-07-09 16:25:06 +0900

    [삼성전기] mysql info.feedback.mode = 'E' 인 케이스 피드백 관련 단어 변경 (개인, 일반, 업무성, 대외비)

--- src/main/java/com/xcurenet/common/util/locale/Prop.java
+++ src/main/java/com/xcurenet/common/util/locale/Prop.java
@@ -6,6 +6,7 @@ import java.util.Locale;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import com.xcurenet.common.util.config.Config;
 import org.springframework.context.MessageSource;
 import org.springframework.context.MessageSourceAware;
 import org.springframework.stereotype.Component;
@@ -46,12 +47,18 @@ public class Prop implements MessageSourceAware {
 		return propFormat(key, Common.getLocale(session), objects);
 	}
 
-	public static String msg(String key) {
-		return msg.getMessage(key, null, Locale.getDefault());
-	}
+	public static String msg(String key) {return msg.getMessage(keyChange(key), null, Locale.getDefault());}
 
-	public static String msg(String key, Locale locale) {
-		return msg.getMessage(key, null, locale);
+	public static String msg(String key, Locale locale) {return msg.getMessage(keyChange(key), null, locale);}
+
+	private static String keyChange(final String key) {
+		String str = key;
+		if(Common.isEquals(Config.getString("info.feedback.mode"), "E")) {
+			if(Common.isOrEquals(str, "condition.info.class1", "condition.info.class2", "condition.info.class3", "condition.info.class4", "condition.info.feedback1", "condition.info.feedback2", "condition.info.feedback3", "condition.info.feedback4")) {
+				str += "E";
+			}
+		}
+		return str;
 	}
 
 	public static void main(String[] args) {
