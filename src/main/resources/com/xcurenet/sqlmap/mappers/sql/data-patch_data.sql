a17543538119dd528fc82470c063d627e3e7ad6c
yn.ahn <yn.ahn@xcurenet.com>
2025-07-18 11:02:14 +0900

    UI_LLM value.analysis 추가

--- src/main/resources/com/xcurenet/sqlmap/mappers/sql/data-patch_data.sql
+++ src/main/resources/com/xcurenet/sqlmap/mappers/sql/data-patch_data.sql
@@ -1043,3 +1043,4 @@ INSERT IGNORE INTO UI_LLM(LLM_CONF,LLM_PROMPT,LLM_MODEL,LLM_CONTENT) VALUES ('co
 INSERT IGNORE INTO UI_LLM(LLM_CONF,LLM_PROMPT,LLM_MODEL,LLM_CONTENT) VALUES ('content.summary','위에 있는 내용을 100자이내로 한글로 요약해죠','gemma2:27b','상세보기 - 내용요약');
 INSERT IGNORE INTO UI_LLM(LLM_CONF,LLM_PROMPT,LLM_MODEL,LLM_CONTENT) VALUES ('content.analysis','위에 있는 내용은 인터넷 패킷데이터를 텍스트로 표현한거야 이 부분을 분석해서 어떤 서비스 인지 한글로 알려죠','gemma2:27b','상세보기 - 내용분석');
 INSERT IGNORE INTO UI_LLM(LLM_CONF,LLM_PROMPT,LLM_MODEL,LLM_CONTENT) VALUES ('url.analysis','위에 있는 통신하는 URL 주소가 어떤 서비스인지 간략하게 알려줄수 있어?','gemma2:27b','상세보기 - URL 분석');
+INSERT IGNORE INTO UI_LLM(LLM_CONF,LLM_PROMPT,LLM_MODEL,LLM_CONTENT) VALUES ('value.analysis','분석 요청을 할게 {{path_prompt}}  <- 주소에서  {{trans_type}}  타입으로 전송된 데이터인데 데이터 형식은 {{key_prompt}}:{{value_prompt}} 이렇게돼  해당 내용이 무슨 기능인지 확인해줘 그리고 해당 내용의 보안취약점도 분석해서 짧게 요약해줘','gemma2:27b','값 분석');
