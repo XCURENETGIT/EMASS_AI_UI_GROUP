a5e687bc6c0dc4cdccf3c19f13e010e0832ddffa
김민경 <mk.kim@xcurenet.com>
2025-12-01 14:23:46 +0900

    [삼성카드고객서비스] 본문 내 서명 탐지 제외 기능 사용 시 불필요 문구가 로깅되는 현상 원인 : HTML 정규 표현식은  &nbsp; 형태를 기준으로 하고 있으나, 원문 데이터에 세미콜론(;)이 누락된 &nbsp 형태로 입력되는 경우 공백이 아닌 일반 문자열로 UI 상 보여지는 문제 수정 : HTML 형식의 데이터가 &nbsp로 들어올 경우, UI 에서 보여지는 과정에서 &nbsp; 로 변환하여 공백으로 인식되도록 수정

--- src/main/java/com/xcurenet/emass/message/web/EmsMessageController.java
+++ src/main/java/com/xcurenet/emass/message/web/EmsMessageController.java
@@ -1394,6 +1394,7 @@ public class EmsMessageController {
 			case MAYBE_HTML:
 				return bodyStr;
 			case HTML:
+				bodyStr = bodyStr.replaceAll("&nbsp(?!;)", "&nbsp;");
 				return bodyStr;
 			case MIME:
 				MimeParser parser = new MimeParser(body, userCharset);
